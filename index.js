1c55c23b4ce6b929d3e36a78e6af178d38f2c24b
Victor Muhoro <victormuhoro7@gmail.com>
2023-12-12 23:47:47 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(ride){
   // Write your code here!
+  
+  if (ride <= 400) {
+    return ("This one is on me!");
+  }
+  else if (ride > 2000 && ride < 2500) {
+    return ("I will gladly take your thirty bucks.");
+  }
+  else if (ride > 2500) {
+    return ("No can do.");
+  }
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
+
+function ternaryCheckCity(city) {
+    return (city == "NYC" ? "Ok, sounds good." : "No go.");  }
+ 
   // Write your code here!
+
+
+
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }
+
 }
+  // Write your code here!
